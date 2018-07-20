96d8d72bf52976a3f31c40aee33cf63fe6108b6a
vepins <vepins>
2018-07-20 11:07:07 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tweets, dependent: :destroy
+
+  validates :username, presence:  true, uniqueness: true
+
+  mount_uploader :avatar, AvatarUploader
+
+end
